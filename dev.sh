b76dc6fe3c78030237a0165236f4b7127e7c3b65
qiwen <yangdongyong@jindanlicai.com>
2017-06-05 11:26:47 +0800

    update

--- dev.sh
+++ dev.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export VTTOP=$(pwd)
-export VTROOT="${VTROOT:-${VTTOP/\/src\/github.com\/flike\/kingshard/}}"
+export VTROOT="${VTROOT:-${VTTOP/\/src\/github.com\/qiwenilli\/kingshard/}}"
 # VTTOP sanity check
-if [[ "$VTTOP" == "${VTTOP/\/src\/github.com\/flike\/kingshard/}" ]]; then
-  echo "WARNING: VTTOP($VTTOP) does not contain src/github.com/flike/kingshard"
+if [[ "$VTTOP" == "${VTTOP/\/src\/github.com\/qiwenilli\/kingshard/}" ]]; then
+  echo "WARNING: VTTOP($VTTOP) does not contain src/github.com/qiwenilli/kingshard"
 fi
 
 export GOTOP=$VTTOP
